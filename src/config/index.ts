a460c1c0a92d902067955b499ff7b76ef9599a54
danghuan <18792809954@163.com>
2022-06-01 22:02:41 +0800

    feat: 1

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,18 +2,30 @@ import yaml from 'yaml'
 import { readFileSync } from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import fs from 'fs-extra';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const filePath = path.resolve(__dirname, '../../config.yaml')
 
-const configFile = readFileSync(filePath, 'utf8')
-
-const config: {
+let config: {
   port: number
   db: {
     path: string
   }
-} = yaml.parse(configFile)
+}
+
+if(fs.existsSync(filePath)){
+  config = yaml.parse(readFileSync(filePath, 'utf8'))
+}else{
+  config= {
+    port: Number(process.env.port) || 3000,
+    db: {
+      path: process.env.dbPath || '/link-db'
+    }
+  }
+}
+
+
 
 export default {
   ...config
